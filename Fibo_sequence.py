cabe6400f5064e867fc141e9a468e750247e8e0b
chaset04 <164583492+chaset04@users.noreply.github.com>
2024-03-23 21:15:31 -0400

    Fibonacci sequence program

---
+++ Fibo_sequence.py
@@ -0,0 +1,15 @@
+def fibo(n):
+	num1 = 0
+	num2 = 1
+	next_num = num2
+	count = 1
+
+	while count <= n:
+		print(next_num, end=' ')
+		count += 1
+		num1, num2 = num2, next_num
+		next_num = num1 + num2
+
+
+n = 5
+fibo(n)
